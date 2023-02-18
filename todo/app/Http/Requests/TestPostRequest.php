5635aabf11503a30e8cc072f915a0d3ffd46ef6a
soga syunto <sogasyunto@soganoMacBook-Air.local>
2023-02-18 20:17:14 +0900

    first-commit

--- todo/app/Http/Requests/TestPostRequest.php
+++ todo/app/Http/Requests/TestPostRequest.php
@@ -1,10 +1,10 @@
 <?php
 
 namespace App\Http\Requests;
-use App\Http\Requests\TodoRequest;
+use App\Http\Requests\TestPostRequest;
 use Illuminate\Foundation\Http\FormRequest;
 
-class TodoRequest extends FormRequest
+class TestPostRequest extends FormRequest
 {
     /**
      * Determine if the user is authorized to make this request.
@@ -23,18 +23,8 @@ class TodoRequest extends FormRequest
      */
     public function rules()
     {
-        return [
-            'content' => 'required| max:20',
-            
+        return ['content_update' => 'required| max:20',
             //
         ];
     }
-
-    public function messages()
-    {
-        return[
-            'content.required' => '・タスクを入力してください',
-            'content.max' => '・タスクは20文字以内で入力してください。',
-        ];
-        }
 }
